4acf6cd85aae041347513530e85671be4956ac6e
Artsiom Pas <akella1997@tut.by>
2017-12-10 19:37:55 +0300

    add fetch

--- src/App.js
+++ src/App.js
@@ -5,15 +5,49 @@ import Profile from './Profile/Profile.js';
 import Tabs from './Tabs/Tabs.js';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: '',
+            name: '',
+            nickname: '',
+            information: ''
+        }
+    }
+
+    componentDidMount() {
+        return fetch('https://api.github.com/users/akella-brest')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    url: responseJson.avatar_url,
+                    name: responseJson.name,
+                    nickname: responseJson.login,
+                    information: responseJson.bio
+                }, function() {
+                    // do something with new state
+                });
+            })
+    }
+
     render() {
         const icon = ['users', 'map-marker', 'envelope', 'link'];
         const rel = ['facebook.com', '', 'mailto:akella1997@tut.by', 'vk.com/artsiom_pas_blr'];
         const text = ['@facebook', 'Brest, Belarus', 'akella1997@tut.by', '@vk'];
         return (
             <div className="App">
-                    <Profile url="profile.jpg" title="profile" name="Artsiom Pas" info="Student" />
+                    <Profile
+                        url={this.state.url}
+                        name={this.state.name}
+                        nickname={this.state.nickname}
+                        information={this.state.information}
+                    />
                     <Tabs />
-                    {icon.map((icon, index) => <Icon icon={icon} rel={rel[index]} text={text[index]}/>)}
+                    {icon.map((icon, index) => <Icon
+                        icon={icon}
+                        rel={rel[index]}
+                        text={text[index]}
+                    />)}
             </div>
         );
     }
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -3,14 +3,12 @@ import './Profile.css';
 
 class Profile extends Component {
     render() {
-        const information = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
-        'Vivamus nec cursus nibh. ']
         return (
             <div className="Profile">
-                <img className="img" src={this.props.url} alt={`${this.props.title} picture`} />
-                <p id="name"> {this.props.name} </p>
-                <p id="status"> {this.props.info} </p>
-                <p id="information">{information}</p>
+                <img className="img" src={this.props.url} alt={'profile picture'} />
+                <p id="name">{this.props.name}</p>
+                <p id="status">{this.props.nickname}</p>
+                <p id="information">{this.props.information}</p>
             </div>
         );
     }
